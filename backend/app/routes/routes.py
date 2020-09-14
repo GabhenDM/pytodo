faae82b2f88a8056eb605f98b4d62f69df0f81b2
Gabriel Henriques <henriques.gabriel@outlook.com>
2020-09-14 04:09:38 -0300

    Updated user CRUD routes

--- backend/app/routes/routes.py
+++ backend/app/routes/routes.py
@@ -8,6 +8,22 @@ def root():
     return jsonify({"message": "Hello World"})
 
 
+@app.route("/users",methods=['GET'])
+def get_users():
+    return users.get_users()
+
+@app.route("/users/<id>",methods=['GET'])
+def get_user(id):
+    return users.get_user(id)
+
 @app.route("/users",methods=['POST'])
 def create_user():
     return users.create_user()
+
+@app.route("/users/<id>",methods=['PUT'])
+def update_user(id):
+    return users.update_user(id)
+
+@app.route("/users/<id>",methods=['DELETE'])
+def delete_user(id):
+    return users.delete_user(id)
--- backend/app/views/users.py
+++ backend/app/views/users.py
@@ -4,6 +4,35 @@ from flask import request, jsonify
 from ..models.users import Users, user_schema, users_schema
 import sys
 
+
+def get_users():
+    users = Users.query.all()
+    if users:
+        result = users_schema.dump(users)
+        return jsonify({'message':'Successfully fetched users','data':result}),200
+
+def get_user(id):
+    user = Users.query.get(id)
+    if not user:
+        result = user_schema.dump(user)
+        return jsonify({'message':'Successfully fetched user','data':result}),200
+    else:
+        return jsonify({'message': 'No such user','data':{}}),404
+
+def delete_user(id):
+    user = Users.query.get(id)
+    if not user:
+        return jsonify({'message': 'No such user','data':{}}),404
+    try:
+        db.session.delete(user)
+        db.session.commit()
+        result = user_schema.dump(user)
+        return jsonify({'message':'Successfully deleted user','data':result}),200     
+    except Exception as e:
+        message = list(e.orig.args)[1]
+        db.session.rollback()
+        return jsonify({'message':'Error deleting user','desc':message}), 500
+    
 def create_user():
     username = request.json['username']
     password = request.json['password']
@@ -21,3 +50,28 @@ def create_user():
         message = list(e.orig.args)[1]
         db.session.rollback()
         return jsonify({'message':'Error creating user','desc':message}), 500
+
+def update_user(id):
+    username = request.json['username']
+    password = request.json['password']
+    name = request.json['name']
+    email = request.json['email']
+
+    user = Users.query.get(id)
+    if not user:
+        return jsonify({'message':"User doesn't exist",'data':{}}),404
+    
+    pass_hash = generate_password_hash(password)
+    try:
+        user.username = username
+        user.password = pass_hash
+        user.name = name
+        user.email = email
+        db.session.commit()
+        result = user_schema.dump(user)
+        result.pop("password",None)
+        return jsonify({'message':'User updated succesfully','data':result}),201
+    except Exception as e:
+        message = list(e.orig.args)[1]
+        db.session.rollback()
+        return jsonify({'message':'Error updating user','desc':message}), 500
