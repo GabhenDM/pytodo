9c7ab407a678f24c7073503d846ab82746f347f5
Gabriel Henriques <henriques.gabriel@outlook.com>
2020-09-15 20:43:00 -0300

    Initial Frontend commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# YATAP - Yet Another Todo App in Python
+
+This is a practice project utilizing Python Flask for the Back-end and VueJS for the Front-End, utilizing key REST functionalities, Fully integrated authentication utilizing JWT. Also with a fully dockerized architecture, including extra services such as: MariaDB for persistence and DB, Redis for JWT Revokation.
---
+++ frontend/pytodo/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <Navbar/>
+    <div>
+      <b-jumbotron header="YATAP" id="jumbotron" lead="Yet Another Todo App in Python">
+        <p>For more information visit website</p>
+        <b-button variant="primary" href="#">More Info</b-button>
+      </b-jumbotron>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/layout/Navbar.vue'
+export default {
+  name: 'App',
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#jumbotron{ 
+  background-color:#666666;
+  color: white;
+  }
+  #jumbotron .btn-primary {
+    background-color: #FE7F2D;
+    color:white;
+    border-color:#FE7F2D;
+    border-radius:10px;
+  }
+  #jumbotron .btn-primary:hover {
+    background-color: #B64A02;
+    border-color: #B64A02
+  }
+html {
+  background-color: #8F8F8F
+}
+</style>
